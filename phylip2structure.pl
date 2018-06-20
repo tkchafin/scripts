11b25dfb12cc5578871f45effe55150237899b67
Tyler K. Chafin <tkchafin@uark.edu>
2018-06-20 12:57:43 -0500

    bug fix when sample not in popmap

--- phylip2structure.pl
+++ phylip2structure.pl
@@ -1,18 +1,18 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 
-use strict; 
-use warnings; 
+use strict;
+use warnings;
 use Getopt::Long;
 use File::Basename;
 
 #Initialize scalars
-my $input; 
+my $input;
 my $popmap;
 my $output="structure.in";
-my $missing="-9";  
-my $suppress=0; 
-my $extra; 
-my $locnames=0; 
+my $missing="-9";
+my $suppress=0;
+my $extra;
+my $locnames=0;
 #Call sub parseArgs to parse command-line arguments
 parseArgs();
 
@@ -24,8 +24,8 @@ if ($suppress == 0){
 
 #Format output if default used
 if ($output eq "structure.in"){
-	my ($filepath, $dirpath) = fileparse ($input); 
-	$output = "$dirpath/$output"; 
+	my ($filepath, $dirpath) = fileparse ($input);
+	$output = "$dirpath/$output";
 };
 
 #Specify iupac abiguity codes and how to write them out to structure file
@@ -42,21 +42,21 @@ W       1 4
 K       3 4
 M       1 2 ";
 
-my %first_line; 
+my %first_line;
 my %second_line;
 
 #Build hashes of above iupac codes
 for my $line (split "\n", $iupac){
-	chomp $line; 
-	my @a = split /\s+/, $line; 
-	$first_line{ $a[0] } = $a[1]; 
+	chomp $line;
+	my @a = split /\s+/, $line;
+	$first_line{ $a[0] } = $a[1];
 	$second_line{ $a[0] } = $a[2];
 
 };
 
 #Store population identifiers for each individual (from popmap)
-my %popmap; 
-my %enum; 
+my %popmap;
+my %enum;
 if ($popmap){
     open ( POPMAP, $popmap) || die "Derp: Can't open $popmap: $!";
     my $popcount = 0;
@@ -82,21 +82,21 @@ open ( OUTFILE, ">", $output) || die "Can't open $output: $!";
 open ( PHY, $input ) || die "Can't open $input: $!";
 
 while ( my $line = <PHY> ){
-	$count++; 	
+	$count++;
 	$count == 1 and next; #Test if $count=1, if so then skip to next iteration
-	
-	#Split each line, store sequence name and sequence 
-	my @b = split /\s+/, $line; 
-	my @seq_array = split //, $b[1]; 
-	
+
+	#Split each line, store sequence name and sequence
+	my @b = split /\s+/, $line;
+	my @seq_array = split //, $b[1];
+
 	#Build first line of structure file, containing "locus IDs"
 	if ($count == 2){
 	    my $locus_names= "\t\t";
 	    for (my $i=1; $i <= scalar @seq_array; $i++){
-		$locus_names .= "$i\t"; 
+		$locus_names .= "$i\t";
 	    }
 	    chop $locus_names;
-	    
+
 	   if ($locnames == 1){
 		 print OUTFILE "$locus_names\n";
 	   }
@@ -106,44 +106,49 @@ while ( my $line = <PHY> ){
 	my $line_2 = "$b[0]\t";
 
 	if ($popmap){
-		#Add pop codes
-		$line_1 .= "$popmap{$b[0]}\t";
-		$line_2 .= "$popmap{$b[0]}\t";
+		if (exists $popmap{$b[0]}){
+			#Add pop codes
+			$line_1 .= "$popmap{$b[0]}\t";
+			$line_2 .= "$popmap{$b[0]}\t";
+		}
+		else{
+			next;
+		}
 	}
 
-	if ($extra){ 
+	if ($extra){
 	   for (my $i=0; $i<$extra; $i++){
-		$line_1 .= "0\t"; 
-		$line_2 .= "0\t"; 
+		$line_1 .= "0\t";
+		$line_2 .= "0\t";
 	    }
 	}
 
 	#Start adding allele data
-	for( my $i=0; $i <= $#seq_array; $i++ ){ 
+	for( my $i=0; $i <= $#seq_array; $i++ ){
 		if ($first_line{ uc $seq_array[$i] }){
 		    $line_1 .= "$first_line{ uc $seq_array[$i] }\t";
-		}else{ 
+		}else{
 		    $line_1 .= "-9\t";
-		} 
-		if ($second_line{ uc $seq_array[$i] }){ 
+		}
+		if ($second_line{ uc $seq_array[$i] }){
 		    $line_2 .= "$second_line{ uc $seq_array[$i] }\t";
-		}else{ 
+		}else{
 		    $line_2 .= "-9\t";
 		}
-  
+
 	}
-	
-	chop $line_1; 
+
+	chop $line_1;
 	chop $line_2;
-	
-	print OUTFILE $line_1, "\n"; 
-	print OUTFILE $line_2, "\n"; 
+
+	print OUTFILE $line_1, "\n";
+	print OUTFILE $line_2, "\n";
 
 }
 
 close PHY;
 close OUTFILE;
-exit;  
+exit;
 
 ############################SUBROUTINES######################################
 
@@ -154,7 +159,7 @@ my $usage= "\nUsage: $0 -i /path/to/phylip -p /path/to/popmap -o /path/to/output
 
 The purpose of this script is to take a phylip-formatted file of concatenated SNPs (such as that output by the program pyRAD) and convert it to a structure-formatted file, with two lines for each individual representing the phased allele, as well as a column representing the a priori population/ locality assignment (as provided by the user in the form of a tab-delimited table).
 
-Format of population map: 
+Format of population map:
 Sample1 	1
 Sample2		1
 Sample3		2
@@ -169,9 +174,10 @@ Required Inputs
 
 Optional inputs
 	-l, --loc	-  Bool, switch on printing of locus names in first row
-	-e, --extra	-  Number of extra columns to insert 
+	-e, --extra	-  Number of extra columns to insert
 	-m, --missing	-  Desired code for missing data [Default is \"-9\"]
 	-q, --quiet	-  Quiet mode; suppress internal warnings
+	-x	- Exlude samples that are NOT in popmap
 
 NOTE: Both gaps and N\'s will be coded as missing data.\n\n";
 
@@ -182,7 +188,7 @@ NOTE: Both gaps and N\'s will be coded as missing data.\n\n";
 	'output|o=s'	=> \$output,
 	'missing|m=s'	=> \$missing,
 	'help|h!'	=> \$help,
-	'extra|e=i'	=> \$extra, 
+	'extra|e=i'	=> \$extra,
 	'loc|l!'	=> \$locnames,
 	'quiet|q!'	=> \$suppress,
 	);
@@ -190,4 +196,3 @@ NOTE: Both gaps and N\'s will be coded as missing data.\n\n";
 	$help == 1 and die "$usage";
 	$input || die "Input not specified!\n$usage";
 };
-	
