73a5b80dc62290f80a08d5c0de865b0fdb9f4484
Tyler Chafin <tkchafin@uark.edu>
2019-12-12 15:46:31 -0600

    type change

---
+++ phylobarcode.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+
+def main():
+	params = parseArgs()
+
+#Object to parse command-line arguments
+class parseArgs():
+	def __init__(self):
+		#Define options
+		try:
+			options, remainder = getopt.getopt(sys.argv[1:], 'h', \
+			["help"])
+		except getopt.GetoptError as err:
+			print(err)
+			self.display_help("\nExiting because getopt returned non-zero exit status.")
+		#Default values for params
+		#Input params
+
+
+
+		#First pass to see if help menu was called
+		for o, a in options:
+			if o in ("-h", "-help", "--help"):
+				self.display_help("Exiting because help menu was called.")
+
+		#Second pass to set all args.
+		for opt, arg_raw in options:
+			arg = arg_raw.replace(" ","")
+			arg = arg.strip()
+			opt = opt.replace("-","")
+			#print(opt,arg)
+			if opt == "h" or opt == "help":
+				continue
+			else:
+				assert False, "Unhandled option %r"%opt
+
+		#Check manditory options are set
+		if not self.files:
+			self.display_help("No files provided.")
+
+
+
+	def display_help(self, message=None):
+		if message is not None:
+			print()
+			print (message)
+		print ("\n<template.py>\n")
+		print("Author: Tyler K Chafin, University of Arkansas")
+		print ("Contact: tkchafin@uark.edu")
+		print ("Description: ")
+		print("""
+
+""")
+		print()
+		sys.exit()
+
+#Call main function
+if __name__ == '__main__':
+    main()
