8d752efea6ba44e9d7be813918c2d7efa8c87ea4
Tyler Chafin <tkchafin@uark.edu>
2020-06-28 11:51:46 -0500

    fasta convert b/n one and multi line formats

--- fastaFormatter.py
+++ fastaFormatter.py
@@ -3,11 +3,77 @@
 import sys
 import os
 import getopt
+from textwrap import wrap
 
 def main():
 	params = parseArgs()
 	
-	
+	if params.many2one:
+		seqs=dict()
+		for f in read_fasta(params.many2one):
+			seqs[f[0]] = f[1]
+		write_fasta(params.out, seqs)
+	elif params.one2many:
+		seqs=dict()
+		for f in read_fasta(params.one2many):
+			seqs[f[0]] = f[1]
+		write_fasta(params.out, seqs, params.width)
+
+#Function to write fasta-formatted sequences
+def write_fasta(f, aln, width=None):
+	with open(f, 'w') as fh:
+		try:
+			for samp in aln.keys():
+				if width:
+					ol = ">" + str(samp) + "\n"
+					chunks=wrap(aln[samp], width=width, break_on_hyphens=False, drop_whitespace=False)
+					for chunk in chunks:
+						ol=ol + str(chunk) + "\n"
+				else:
+					ol = ">" + str(samp) + "\n" + str(aln[samp]) + "\n"
+				fh.write(ol)
+		except IOError as e:
+			print("Could not read file %s: %s"%(f,e))
+			sys.exit(1)
+		except Exception as e:
+			print("Unexpected error reading file %s: %s"%(f,e))
+			sys.exit(1)
+		finally:
+			fh.close()
+
+#Read samples as FASTA. Generator function
+def read_fasta(fas):
+	if os.path.exists(fas):
+		with open(fas, 'r') as fh:
+			try:
+				contig = ""
+				seq = ""
+				for line in fh:
+					line = line.strip()
+					if not line:
+						continue
+					#print(line)
+					if line[0] == ">": #Found a header line
+						#If we already loaded a contig, yield that contig and
+						#start loading a new one
+						if contig:
+							yield([contig,seq]) #yield
+							contig = "" #reset contig and seq
+							seq = ""
+						split_line = line.split()
+						contig = (split_line[0].replace(">",""))
+					else:
+						seq += line
+				#Iyield last sequence, if it has both a header and sequence
+				if contig and seq:
+					yield([contig,seq])
+			except IOError:
+				print("Could not read file ",fas)
+				sys.exit(1)
+			finally:
+				fh.close()
+	else:
+		raise FileNotFoundError("File %s not found!"%fas)
 
 #Object to parse command-line arguments
 class parseArgs():
